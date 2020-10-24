759b6f569a58c84959f28bfd210d6ef7b818cb99
Archery2000 <ongyuhann@hotmail.com>
2020-10-25 01:14:25 +0800

    Add Support for Async Result Collections

--- CSharp-Result/AsyncResultCollectionExtensions.cs
+++ CSharp-Result/AsyncResultCollectionExtensions.cs
@@ -1,56 +1,348 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CSharp_Result
 {
-    public static class AsyncResultCollectionExtensions
+    public static class AsyncResultCollectionExtensions()
     {
-        public static async Task<bool> AllSucceed<T>(this Task<IEnumerable<Result<T>>> list) 
-            where T : notnull
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <param name="results"></param>
+       /// <typeparam name="TSucc"></typeparam>
+       /// <returns></returns>
+        public static async Task<IEnumerable<Result<TSucc>>> AwaitAll<TSucc>(this IEnumerable<Task<Result<TSucc>>> results)
         {
-            return (await list).AllSucceed();
+           var awaited = await Task.WhenAll(results.ToList());
+           return awaited.AsEnumerable();
+        }
+        
+        public static async Task<Result<TSucc>> AwaitAny<TSucc>(this IEnumerable<Task<Result<TSucc>>> results)
+        {
+           return (await Task.WhenAny(results)).Result;
         }
 
-        public static async Task<bool> AnySucceed<T>(this Task<IEnumerable<Result<T>>> list) 
-            where T : notnull
+        public static async IAsyncEnumerable<Result<TSucc>> ToIAsyncEnumerable<TSucc>(
+           this IEnumerable<Task<Result<TSucc>>> results)
         {
-            return (await list).AnySucceed();
+           foreach (var result in results)
+           {
+              yield return await result;
+           }
+        }
+        
+      /// <summary>
+      /// Executes Async Do on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">The type of the result of the computation (unused)</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> DoAwaitEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Task<Result<TResult>>> function) 
+         where TSucc : notnull
+         where TResult : notnull
+        {
+           return results.Select(x => x.DoAwait(function));
         }
 
-        public static async Task<bool> AllFail<T>(this Task<IEnumerable<Result<T>>> list) 
-            where T : notnull
-        {
-            return (await list).AllFail();
-        }
+      /// <summary>
+      /// Executes Async Do on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">The type of the result of the computation (unused)</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> DoAwaitEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Task<TResult>> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+         where TResult : notnull
+      {
+         return results.DoAwaitEach(function.ToAsyncResultFunc(mapException));
+      }
+      
+      /// <summary>
+      /// Executes Async Do on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> DoAwaitEach<TSucc>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Task> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+      {
+         return results.DoAwaitEach(function.ToAsyncResultFunc(mapException));
+      }
+      
+      /// <summary>
+      /// Executes Do on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">The type of the result of the computation (unused)</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> DoEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Result<TResult>> function) 
+         where TSucc : notnull
+      {
+         return results.Select(x => x.Do(function));
+      }
 
-        public static async Task<bool> AnyFail<T>(this Task<IEnumerable<Result<T>>> list) 
-            where T : notnull
-        {
-            return (await list).AnyFail();
-        }
-        public static async Task<IEnumerable<Exception>> GetFailures<T>(this Task<IEnumerable<Result<T>>> list) 
-            where T : notnull
-        {
-            return (await list).GetFailures();
-        }
+      /// <summary>
+      /// Executes Do on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">The type of the result of the computation (unused)</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> DoEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, TResult> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+      {
+         return results.DoEach(function.ToResultFunc(mapException));
+      }
+      
+      /// <summary>
+      /// Executes Do on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> DoEach<TSucc>(this IEnumerable<Task<Result<TSucc>>> results, Action<TSucc> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+      {
+         return results.DoEach(function.ToResultFunc(mapException));
+      }
 
-        public static async Task<IEnumerable<T>> GetSuccesses<T>(this Task<IEnumerable<Result<T>>> list) 
-            where T : notnull
-        {
-            return (await list).GetSuccesses();
-        }
+      /// <summary>
+      /// Executes Async Then on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">The type of the result of the computation (unused)</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TResult>>> ThenAwaitEach<TSucc, TResult>(
+         this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Task<Result<TResult>>> function)
+      {
+         return results.Select(x => x.ThenAwait(function));
+      }
+      
+      /// <summary>
+      /// Executes Async Then on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">The type of the result of the computation (unused)</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TResult>>> ThenAwaitEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Task<TResult>> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+         where TResult : notnull
+      {
+         return results.ThenAwaitEach(function.ToAsyncResultFunc(mapException));
+      }
+      
+      /// <summary>
+      /// Executes Async Then on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<Unit>>> ThenAwaitEach<TSucc>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Task> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+      {
+         return results.ThenAwaitEach(function.ToAsyncResultFunc(mapException));
+      }
+      
+      /// <summary>
+      /// Executes Then on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">The type of the result of the computation (unused)</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TResult>>> ThenEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Result<TResult>> function) 
+         where TSucc : notnull
+         where TResult : notnull
+      {
+         return results.Select(x => x.Then(function));
+      }
+      
+      /// <summary>
+      /// Executes Then on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">The type of the result of the computation (unused)</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TResult>>> ThenEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, TResult> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+         where TResult : notnull
+      {
+         return results.ThenEach(function.ToResultFunc(mapException));
+      }
+      
+      /// <summary>
+      /// Executes Then on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<Unit>>> ThenEach<TSucc>(this IEnumerable<Task<Result<TSucc>>> results, Action<TSucc> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+      {
+         return results.ThenEach(function.ToResultFunc(mapException));
 
-        public static async Task<Result<IEnumerable<T>>> ToResultOfSeq<T>(this Task<IEnumerable<Result<T>>> list) 
-            where T : notnull
-        {
-            return (await list).ToResultOfSeq();
-        }
+      }
+      
+      /// <summary>
+      /// Executes Async If on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> IfAwaitEach<TSucc>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Task<Result<bool>>> function) 
+         where TSucc : notnull
+      {
+         return results.Select(x => x.IfAwait(function));
+      }
 
-        public static async Task<IEnumerable<Result<T>>> ToSeqOfResults<T>(this Task<Result<IEnumerable<T>>> list) 
-            where T : notnull
-        {
-            return (await list).ToSeqOfResults();
-        }
+      /// <summary>
+      /// Executes Async If on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> IfAwaitEach<TSucc>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Task<bool>> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+      {
+         return results.IfAwaitEach(function.ToAsyncResultFunc(mapException));
+      }
+
+      /// <summary>
+      /// Executes If on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> If<TSucc>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, Result<bool>> function) 
+         where TSucc : notnull
+      {
+         return results.Select(x => x.If(function));
+      }
+
+      /// <summary>
+      /// Executes If on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="function">The function to execute</param>
+      /// <param name="mapException">The mapping function for the error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<Result<TSucc>>> If<TSucc>(this IEnumerable<Task<Result<TSucc>>> results, Func<TSucc, bool> function, Func<Exception, Exception> mapException) 
+         where TSucc : notnull
+      {
+         return results.If(function.ToResultFunc(mapException));
+      }
+
+      /// <summary>
+      /// Executes Async Match on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="Success">The function to execute if Result is Success</param>
+      /// <param name="Failure">The function to execute if Result is Error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <typeparam name="TResult">Return type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<TResult>> MatchAwaitEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results,
+         Func<TSucc, Task<TResult>> Success, Func<Exception, Task<TResult>> Failure)
+      {
+         return results.Select(x => x.MatchAwait(Success, Failure));
+      }
+        
+      /// <summary>
+      /// Executes Async Match on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="Success">The function to execute if Result is Success</param>
+      /// <param name="Failure">The function to execute if Result is Error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task> MatchAwaitEach<TSucc>(this IEnumerable<Task<Result<TSucc>>> results,
+         Func<TSucc, Task> Success, Func<Exception, Task> Failure)
+      {
+         return results.Select(x => x.MatchAwait(Success, Failure));
+      }
+      
+      /// <summary>
+      /// Executes Match on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="Success">The function to execute if Result is Success</param>
+      /// <param name="Failure">The function to execute if Result is Error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task<TResult>> MatchEach<TSucc, TResult>(this IEnumerable<Task<Result<TSucc>>> results,
+         Func<TSucc, TResult> Success, Func<Exception, TResult> Failure)
+      {
+         return results.Select(x => x.Match(Success, Failure));
+      }
+        
+      /// <summary>
+      /// Executes Match on each element of the collection
+      /// </summary>
+      /// <param name="results">Input Async Result Collection</param>
+      /// <param name="Success">The function to execute if Result is Success</param>
+      /// <param name="Failure">The function to execute if Result is Error</param>
+      /// <typeparam name="TSucc">Input type</typeparam>
+      /// <returns>Collection after executing function on each element</returns>
+      public static IEnumerable<Task> MatchEach<TSucc>(this IEnumerable<Task<Result<TSucc>>> results,
+         Action<TSucc> Success, Action<Exception> Failure)
+      {
+         return results.Select(x => x.Match(Success, Failure));
+      }
+      
+      /// <summary>
+      /// Converts a Result Collection into an Async Result Collection
+      /// </summary>
+      /// <param name="results">Result Collection to convert</param>
+      /// <typeparam name="TSucc">Type contained in Result</typeparam>
+      /// <returns>Collection of Async Results</returns>
+      public static IEnumerable<Task<Result<TSucc>>> ToAsyncResultCollection<TSucc>(this IEnumerable<Result<TSucc>> results)
+      {
+         return results.Select(x => x.ToAsyncResult());
+      }
+      
+      /// <summary>
+      /// Converts a Collection into an Async Result Collection
+      /// </summary>
+      /// <param name="results">Result Collection to convert</param>
+      /// <typeparam name="TSucc">Type contained in Result</typeparam>
+      /// <returns>Collection of Async Results</returns>
+      public static IEnumerable<Task<Result<TSucc>>> ToAsyncResultCollection<TSucc>(this IEnumerable<TSucc> results)
+      {
+         return results.Select(x => x.ToAsyncResult());
+      }
     }
 }
