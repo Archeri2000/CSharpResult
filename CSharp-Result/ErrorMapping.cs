19ac0cca992f86e21c5d406fea5d8ec0c1020908
Archeri2000 <cheri.yuhann@outlook.com>
2022-09-29 23:08:51 +0200

    [Docs] added documentation for miscellaneous utilities

--- CSharp-Result/ErrorMapping.cs
+++ CSharp-Result/ErrorMapping.cs
@@ -2,24 +2,52 @@ using System;
 
 namespace CSharp_Result
 {
+    /// <summary>
+    /// Static class to hold methods related to mapping thrown Exceptions
+    /// </summary>
     public static class Errors
     {
+        /// <summary>
+        /// Predicate taking a thrown Exception and returning true if it should be mapped to a Result.
+        /// </summary>
         public delegate bool ExceptionFilter(Exception e);
+        
+        /// <summary>
+        /// Using this ExceptionFilter, any thrown Exceptions will be rethrown
+        /// </summary>
+        /// <param name="e">Thrown Exception</param>
+        /// <returns>False always</returns>
         public static bool MapNone(Exception e)
         {
             return false;
         }
 
+        /// <summary>
+        /// Using this ExceptionFilter, any thrown Exceptions will be converted to a Result
+        /// </summary>
+        /// <param name="e">Thrown Exception</param>
+        /// <returns>True always</returns>
         public static bool MapAll(Exception e)
         {
             return true;
         }
 
+        /// <summary>
+        /// Using this ExceptionFilter, thrown Exceptions are rethrown unless they match the type T.
+        /// </summary>
+        /// <typeparam name="T">Exception type to match</typeparam>
+        /// <returns>ExceptionFilter which returns true when Exception is T</returns>
         public static ExceptionFilter MapIfExceptionIs<T>()
         {
             return e => e is T;
         }
 
+        /// <summary>
+        /// Extension for MapIfExceptionIs to allow it to match more than one type.
+        /// </summary>
+        /// <param name="filter">Existing ExceptionFilter</param>
+        /// <typeparam name="T">Exception type to match</typeparam>
+        /// <returns>ExceptionFilter which returns true when it matches any of the types</returns>
         public static ExceptionFilter Or<T>(this ExceptionFilter filter)
         {
             return e => filter(e) || e is T;
@@ -34,7 +62,8 @@ namespace CSharp_Result
         /// <summary>
         /// Creates a new AssertionException
         /// </summary>
-        public AssertionException():base(){}
+        public AssertionException()
+        {}
 
         /// <summary>
         /// Creates a new AssertionException that captures the exception message
--- CSharp-Result/Utils.cs
+++ CSharp-Result/Utils.cs
@@ -2,23 +2,49 @@ using System;
 
 namespace CSharp_Result
 {
+    /// <summary>
+    /// Miscellaneous common utility methods that can be used in Result piping.
+    /// </summary>
     public static class Utils
     {
+        /// <summary>
+        /// Function that returns its input as its output. This can be useful in If expressions for one of the branches.
+        /// </summary>
+        /// <param name="t">Input object</param>
+        /// <typeparam name="T">Type of the input object</typeparam>
+        /// <returns>Input object</returns>
         public static T Identity<T>(this T t)
         {
             return t;
         }
 
+        /// <summary>
+        /// Predicate Constructor that checks if object is a certain type.
+        /// </summary>
+        /// <typeparam name="T">Type to match</typeparam>
+        /// <returns>Predicate that returns true if object matches T</returns>
         public static Func<object, bool> Is<T>()
         {
             return o => o is T;
         }
 
+        /// <summary>
+        /// Extension to Is predicate constructor, that also checks if object is a certain type.
+        /// </summary>
+        /// <param name="pred">Existing Predicate</param>
+        /// <typeparam name="T">Type to match</typeparam>
+        /// <returns>Predicate that returns true if object matches any of the types</returns>
         public static Func<object, bool> Or<T>(this Func<object, bool> pred)
         {
             return o => pred(o) || o is T;
         }
 
+        /// <summary>
+        /// Tries to cast input object to type T, returning an InvalidCastException result if it fails.  
+        /// </summary>
+        /// <param name="o">Input object</param>
+        /// <typeparam name="T">Type to cast to</typeparam>
+        /// <returns>Result holding either input object cast to T or InvalidCastException.</returns>
         public static Result<T> As<T>(this object o)
         {
             if (o is T t) return t;
